8e8dfc65b09731674e926c7eacc8d3bf8af0ee47
BNNARAJ <bnnaraj4192@gmail.com>
2023-10-29 15:27:41 +0530

    user_freindly_and_better_error_handling

--- Calculator.py
+++ Calculator.py
@@ -1,47 +1,48 @@
-# This function adds two numbers
 def add(x, y):
-   return x + y 
+    return x + y
 
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
-    return x / y
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero."
 
-# This function modulus two numbers
 def modulus(x, y):
-    return x % y
+    if y != 0:
+        return x % y
+    else:
+        return "Error: Cannot calculate modulus with zero."
 
-# This function exponentialize two numbers
-def exponentation(x, y):
-    return(x ** y)
+def exponentiation(x, y):
+    return x ** y
 
-# This function floor divide two numbers
 def floordivide(x, y):
-    return(x // y)
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-print("5.Modulus")
-print("6.Exponentialize")
-print("7.Floor divide")
+    if y != 0:
+        return x // y
+    else:
+        return "Error: Cannot floor divide by zero."
 
 while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4/5/6/7): ")
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Modulus")
+    print("6. Exponentialize")
+    print("7. Floor divide")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4','5','6','7'):
+    # take input from the user
+    choice = input("Enter choice (1/2/3/4/5/6/7): ")
+
+    # check if choice is one of the options
+    if choice in ('1', '2', '3', '4', '5', '6', '7'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -49,33 +50,30 @@ while True:
             print("Invalid input. Please enter a number.")
             continue
 
-        if choice == '1': 
-                print(num1, "+", num2, "=", add(num1, num2))
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-            
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+
         elif choice == '5':
-            print(num1, "%", num2, "=", modulus(num1, num2))
+            print(f"{num1} % {num2} = {modulus(num1, num2)}")
 
         elif choice == '6':
-            print(num1, "**", num2, "=", exponentation(num1, num2))
+            print(f"{num1} ** {num2} = {exponentiation(num1, num2)}")
 
         elif choice == '7':
-            print(num1, "//", num2, "=", floordivide(num1, num2))
-        
+            print(f"{num1} // {num2} = {floordivide(num1, num2)}")
+
         # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-        else:
-            print("Invalid Input. Returning to start")
+        next_calculation = input("Do you want to perform another calculation? (yes/no): ").lower()
+        if next_calculation != "yes":
+            break
     else:
         print("Invalid Input")
